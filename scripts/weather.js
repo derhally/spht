4b2b438d5ac17a2856385fadfb68c919339f17d0
Zeid Derhally <derhally@gmail.com>
2020-02-09 09:31:09 -0500

    Support setting default weather location

--- scripts/weather.js
+++ scripts/weather.js
@@ -13,6 +13,8 @@
 //   !weather <city>  - Look up the temperature and humidity for the city
 //
 
+const Settings = require('../lib/settings.js')
+
 process.env.HUBOT_WEATHER_API_URL = (process.env.HUBOT_WEATHER_API_URL != null) ?
   process.env.HUBOT_WEATHER_API_URL : 'https://community-open-weather-map.p.rapidapi.com'
 
@@ -22,168 +24,188 @@ process.env.HUBOT_WEATHER_UNITS = (process.env.HUBOT_WEATHER_UNITS != null) ?
 process.env.RAPIDAPI_KEY = (process.env.RAPIDAPI_KEY != null) ?
   process.env.RAPIDAPI_KEY : ''
 
-const WeatherUserPrefKey = "weather-default-loc";
-
-function get_weather_location_for_user(user) {
-  return robot.vault.forUser(user).get(WeatherUserPrefKey)
-}
-
-function get_weather_location_for_user(user, location) {
-  return robot.vault.forUser(user).get(WeatherUserPrefKey, location)
-}
-
-
-function get_emote_for_code(code) {
-
-  if (code >= 200 && code < 300)
-    return ":thunder_cloud_rain:"
-
-  if (code >= 300 && code < 400)
-    return ":cloud_rain:"
-
-  if (code >= 500 && code < 600)
-    return ":cloud_rain:"
-
-  if (code >= 600 && code < 700)
-    return ":snowflake:"
-
-  if (code == 762)
-    return ":volcano:"
-
-  if (code == 781)
-    return ":cloud_tornado:"
-
-  if (code == 800)
-    return ":sunny:"
-
-  if (code >= 801 && code < 805)
-    return ":cloud:"
-
-  return ""
-}
-
-function fetch_weather(robot, msg, query) {
-
-  const isForecast = (typeof query.cnt === 'undefined') ? false : true;
-
-  url = process.env.HUBOT_WEATHER_API_URL;
-  url += isForecast ? "/forecast/daily" : "/weather";
-
-  new Promise((resolve, reject) =>
-      robot.http(url)
-      .header('x-rapidapi-host', 'community-open-weather-map.p.rapidapi.com')
-      .header('X-RapidAPI-Key', process.env.RAPIDAPI_KEY)
-      .query(query)
-      .get()((err, response, body) => {
-        if (err != null) {
-          reject(err);
-        } else if (response.statusCode === 200) {
-          resolve(body);
-        } else {
-          console.log("unknown error")
-          json = JSON.parse(body);
-          reject(json.message);
-        }
-      })
-    )
-    //parse to js object
-    .then(body => {
-      console.log(body)
-      return JSON.parse(body)
-    })
-    .then(json => isForecast ? response_forecast(msg, json) : respond_current_weather(msg, json))
-    .catch(err => msg.reply('Cannot compute: ' + err));
-}
-
-function getDescription(weather) {
-  return weather.description.charAt(0).toUpperCase() + weather.description.slice(1);
-}
-
-function response_forecast(msg, json) {
-  m = `${json.cnt} day forecast for ${json.city.name}:`
-  json.list.forEach((value) => {
-    var date = new Date(value.dt * 1000);
-    m += "\n   " + `${date.getMonth()+1}\\${date.getDate()}: Low: ${Math.round(value.temp.min)} High: ${Math.round(value.temp.max)} Humidity: ${value.humidity}%`
-  });
-  msg.send(m)
-}
-
-function respond_current_weather(msg, json) {
-  m = `It is currently ${Math.round(json.main.temp)}f and feels like ${Math.round(json.main.feels_like)}f in ${json.name}!`
-  m += "\n   " + `${get_emote_for_code(json.weather[0].id)} ${getDescription(json.weather[0])}`
-  m += "\n   " + `Humidity: ${json.main.humidity}%`;
-  m += "\n   " + `Lows ${Math.round(json.main.temp_min)}f High ${Math.round(json.main.temp_max)}f`
-  msg.send(m)
-}
-
-function fetch_weather_byname(robot, msg, name, days) {
-  query = {
-    units: process.env.HUBOT_WEATHER_UNITS,
-    q: name
-  }
-
-  if (days > 0) {
-    query.cnt = days
-  }
-
-  fetch_weather(robot, msg, query);
-}
-
-function fetch_weather_byzip(robot, msg, zipCode, days) {
-  query = {
-    units: process.env.HUBOT_WEATHER_UNITS,
-    zip: zipCode,
-  }
-
-  if (days > 0) {
-    query.cnt = days
-  }
-
-  fetch_weather(robot, msg, query);
-}
-
-function get_start_end_timerange(days) {
-  let startDate = new Date();
-  startDate.setDate(startDate.getDate() + 1);
-  startDate.setHours(0, 0, 0, 0);
-  endDate.setDate(endDate.getDate() + days);
-  endDate.setHours(23, 59, 59, 999);
-  return {
-    start: startDate.valueOf(),
-    end: endDate.valueOf()
-  };
-};
+const WeatherUserPrefKey = "default_weather_loc_";
+const DefaultForecastDays = 3;
 
 module.exports = function (robot) {
-  robot.hear(/!weather (.*)/i, function (msg) {
-    msg.finish();
+  function get_weather_location_for_user(user) {
+    return robot.vault.forUser(user).get(WeatherUserPrefKey)
+  }
 
-    const match = msg.match[1].trim();
+  function get_weather_location_for_user(user, location) {
+    return robot.vault.forUser(user).get(WeatherUserPrefKey, location)
+  }
+
+  function get_emote_for_code(code) {
+
+    if (code >= 200 && code < 300)
+      return ":thunder_cloud_rain:"
+
+    if (code >= 300 && code < 400)
+      return ":cloud_rain:"
+
+    if (code >= 500 && code < 600)
+      return ":cloud_rain:"
+
+    if (code >= 600 && code < 700)
+      return ":snowflake:"
+
+    if (code == 762)
+      return ":volcano:"
+
+    if (code == 781)
+      return ":cloud_tornado:"
+
+    if (code == 800)
+      return ":sunny:"
+
+    if (code >= 801 && code < 805)
+      return ":cloud:"
+
+    return ""
+  }
+
+  function fetchWeather(robot, msg, query) {
+
+    const isForecast = (typeof query.cnt === 'undefined') ? false : true;
+
+    url = process.env.HUBOT_WEATHER_API_URL;
+    url += isForecast ? "/forecast/daily" : "/weather";
+
+    new Promise((resolve, reject) =>
+        robot.http(url)
+        .header('x-rapidapi-host', 'community-open-weather-map.p.rapidapi.com')
+        .header('X-RapidAPI-Key', process.env.RAPIDAPI_KEY)
+        .query(query)
+        .get()((err, response, body) => {
+          if (err != null) {
+            reject(err);
+          } else if (response.statusCode === 200) {
+            resolve(body);
+          } else {
+            console.log("unknown error")
+            json = JSON.parse(body);
+            reject(json.message);
+          }
+        })
+      )
+      //parse to js object
+      .then(body => {
+        console.log(body)
+        return JSON.parse(body)
+      })
+      .then(json => isForecast ? renderForecast(msg, json) : renderCurrent(msg, json))
+      .catch(err => msg.reply('Cannot compute: ' + err));
+  }
+
+  function getDescription(weather) {
+    return weather.description.charAt(0).toUpperCase() + weather.description.slice(1);
+  }
+
+  function renderForecast(msg, json) {
+    m = `${json.cnt} day forecast for ${json.city.name}:`
+    json.list.forEach((value) => {
+      var date = new Date(value.dt * 1000);
+      m += "\n   " + `${date.getMonth()+1}\\${date.getDate()}: Low: ${Math.round(value.temp.min)} High: ${Math.round(value.temp.max)} Humidity: ${value.humidity}%`
+    });
+    msg.send(m)
+  }
+
+  function renderCurrent(msg, json) {
+    m = `It is currently ${Math.round(json.main.temp)}f and feels like ${Math.round(json.main.feels_like)}f in ${json.name}!`
+    m += "\n   " + `${get_emote_for_code(json.weather[0].id)} ${getDescription(json.weather[0])}`
+    m += "\n   " + `Humidity: ${json.main.humidity}%`;
+    m += "\n   " + `Lows ${Math.round(json.main.temp_min)}f High ${Math.round(json.main.temp_max)}f`
+    msg.send(m)
+  }
+
+  function fetchWeatherByName(robot, msg, name, days) {
+    query = {
+      units: process.env.HUBOT_WEATHER_UNITS,
+      q: name
+    }
+
+    if (days > 0) {
+      query.cnt = days
+    }
+
+    fetchWeather(robot, msg, query);
+  }
+
+  function fetchWeatherByZip(robot, msg, zipCode, days) {
+    query = {
+      units: process.env.HUBOT_WEATHER_UNITS,
+      zip: zipCode,
+    }
+
+    if (days > 0) {
+      query.cnt = days
+    }
+
+    fetchWeather(robot, msg, query);
+  }
+
+  function handleRequest(robot, msg, location, days) {
     const zip_regex = /^\d{5}$/;
-
-    if (zip_regex.test(match))
-      fetch_weather_byzip(robot, msg, match, 0);
+    if (zip_regex.test(location))
+      fetchWeatherByZip(robot, msg, location, days);
     else
-      fetch_weather_byname(robot, msg, match, 0);
+      fetchWeatherByName(robot, msg, location, days);
+  }
+
+  function getUserPreferredLocation(userSettings) {
+    return userSettings.default_weather_loc || null;
+  }
+
+  function setUserPreferredLocation(userSettings, location) {
+    userSettings.default_weather_loc = location;
+  }
+
+  ///////////////
+  // LISTENERS //
+  ///////////////
+  robot.hear(/!weather (.*)/i, function (res) {
+    res.finish();
+
+    const location = res.match[1].trim();
+    handleRequest(robot, res, location, 0);
   });
 
-  robot.hear(/!forecast (.+)$/i,
-    function (msg) {
-      msg.finish();
+  robot.hear(/!forecast (.+)$/i, function (res) {
+    res.finish();
 
-      match = msg.match[1].trim();
-      const zip_regex = /^\d{5}$/;
+    location = res.match[1].trim();
 
-      days = 3
-      daysMatches = match.match(/(?:\s)([1-5])$/);
-      if (daysMatches) {
-        days = parseInt(daysMatches[1]);
-        match = match.substring(0, match.length - daysMatches[0].length);
-      }
+    let days = DefaultForecastDays
+    daysMatches = location.match(/(?:\s)([1-5])$/);
+    if (daysMatches) {
+      days = parseInt(daysMatches[1]);
+      location = location.substring(0, location.length - daysMatches[0].length);
+    }
 
-      if (zip_regex.test(match))
-        fetch_weather_byzip(robot, msg, match, days);
-      else
-        fetch_weather_byname(robot, msg, match, days);
-    });
+    handleRequest(robot, res, location, days);
+  });
+
+  robot.hear(/!set my weather location to (.*)$/i, function (res) {
+    location = res.match[1].trim();
+    const userSettings = Settings.forUser(robot, res.message.user.id);
+    setUserPreferredLocation(userSettings, location);
+    msg.send(`I've saved it ${rs.message.user.name}.`)
+  });
+
+  robot.hear(/!weather$/i, function (res) {
+    res.finish();
+
+    const userSettings = Settings.forUser(robot, res.message.user.id);
+    var userPrefLocation = getUserPreferredLocation(userSettings)
+    handleRequest(robot, msg, userPrefLocation, 0);
+  });
+
+  robot.hear(/!forecast$/i, function (res) {
+    res.finish();
+
+    const userSettings = Settings.forUser(robot, res.message.user.id);
+    var userPrefLocation = getUserPreferredLocation(userSettings)
+    handleRequest(robot, msg, userPrefLocation, DefaultForecastDays);
+  });
 }
