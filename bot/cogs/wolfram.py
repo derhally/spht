d35697116a5df072c3c341ec82043cac87227ca7
Zeid Derhally <derhally@gmail.com>
2021-03-22 12:35:57 -0400

    fix some issues with wolfram responses

--- bot/cogs/wolfram.py
+++ bot/cogs/wolfram.py
@@ -15,24 +15,41 @@ class Wolfram(commands.Cog):
         self.wolf_client = wolframalpha.Client(app_id)
 
     @staticmethod
-    def render(dict):
-        results = 0
-        for pod in list(dict['pod']):
-            if pod['@id'] == 'Input':
-                input_value = pod['subpod']['img']['@title']
-            elif pod['@id'] == 'Result':
-                result_value = pod['subpod']['img']['@title']
-                results = 1
-            elif pod['@id'] == 'DecimalApproximation' and results == 0:
-                result_value = pod['subpod']['img']['@title']
-                results = 1
-            elif results == 0:
-                result_value = pod['subpod']['img']['@title']
+    def render(query, res, dict):
         print_embed = discord.Embed(
             title='Results',
             description='Wolfram|Alpha',
             color=discord.Colour.purple()
         )
+        result_value = ""
+        results = 0
+        if 'pod' in dict:
+            for pod in list(dict['pod']):
+                if pod['@id'] == 'Input':
+                    input_value = pod['subpod']['img']['@title']
+                elif pod['@id'] == 'Result':
+                    result_value = pod['subpod']['img']['@title']
+                    results = 1
+                elif pod['@id'] == 'DecimalApproximation' and results == 0:
+                    result_value = pod['subpod']['img']['@title']
+                    results = 1
+                elif results == 0:
+                    if isinstance(pod['subpod'], wolframalpha.Subpod):
+                        result_value = pod['subpod']['img']['@title']
+                    else:
+                        for subpod in pod['subpod']:
+                            if result_value != "":
+                                result_value += "\n"
+                            result_value += subpod['img']['@title']
+
+        elif hasattr(res, "didyoumeans"):
+            input_value = query
+            if res.didyoumeans["@count"] == '1':
+                didyoumean = res.didyoumeans["didyoumean"]["#text"]
+            else:
+                didyoumean = res.didyoumeans["didyoumean"][0]["#text"]
+            result_value = f"did you mean '{didyoumean}'?"
+
         print_embed.set_thumbnail(url='https://i.imgur.com/cXwo5bz.png')
         print_embed.add_field(name='Input:', value=input_value, inline=False)
         print_embed.add_field(name='Result:', value=result_value, inline=False)
@@ -47,7 +64,7 @@ class Wolfram(commands.Cog):
             if res_dict['@error'] == 'true':
                 await ctx.send("I have no idea.")
                 return
-            await ctx.send(embed=Wolfram.render(res_dict))
+            await ctx.send(embed=Wolfram.render(query, res, res_dict))
         except Exception as e:
             await ctx.send(f"Error: {e}")
 
